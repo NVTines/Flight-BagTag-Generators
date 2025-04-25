ee33ec9006f5ca9adc95cfee6e71a4e1a9d04ed9
NVTines <ngovantin280702@gmail.com>
2025-04-25 18:41:06 +0700

    init

--- pages/form1.js
+++ pages/form1.js
@@ -1,34 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Form1 from "../components/Form1"
-import Loading from '../components/loading'
 
 function Form() {
-
-  const [loader, setLoader] = useState(true)
-  const [tick, setTick] = useState(true)
-
-  useEffect(() => {
-
-    setInterval(
-      () => setLoader(false)
-      , 6000);
-
-    // setTimeout(
-    //     () => setLoader(false),
-    //     6000
-    // );
-
-
-
-  }, [])
-
   return (
     <div>
-      {loader ?
-        <Loading />
-        :
-        <Form1 />
-      }
+        <Form1/>
     </div>
   )
 }
